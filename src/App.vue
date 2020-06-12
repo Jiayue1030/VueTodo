11b4ac70a26bad1d3e38f31203cabd18a85ca526
Jiayue1030 <jiayue1030@gmail.com>
2020-06-12 17:32:41 +0800

    Vue Todolist

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <TodoList />
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import TodoList from './components/TodoList.vue';
+
+library.add(faCheck);
+
+export default {
+  name: 'app',
+  components: {
+    TodoList
+  }
+}
+
+Vue.use(BootstrapVue);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,167 @@
+<template>
+  <div>
+    <h1>Vue Todo List</h1>
+    <input type="text" placeholder="Add a word..." v-model="word">
+    <button class="addButton" @click="addWord">Add</button>
+
+    <b-list-group class="listGroup">
+      <b-list-group-item
+        class="listGroupItem"
+        v-for="(item, index) in filteredArray"
+        v-bind:key="index"
+        :class="{ active: item.done === true }"
+      >
+        <span>{{ index + 1 }} -</span>
+        {{ item.text }}
+        <button class="removeButton" @click="removeItem(index);">X</button>
+        <font-awesome-icon class="checkIcon" icon="check" @click="changeStatus(item);"/>
+      </b-list-group-item>
+    </b-list-group>
+
+    <ul class="filter">
+      <li @click="filterArray('all')">All</li>
+      <li @click="filterArray('active')">Active</li>
+      <li @click="filterArray('complete')">Completed</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoList",
+  created: function(){
+      this.filteredArray = [...this.todoArray];
+  },
+  data() {
+    return {
+      count: 0,
+      word: "",
+      todoArray: [
+        { text: "Study", done: false },
+        { text: "Learn something", done: false },
+        { text: "Drink water", done: false },
+        { text: "Sleep early", done: false }
+      ],
+      filteredArray: []
+    };
+  },
+  methods: {
+    addWord: function() {
+      if (this.word !== "") {
+        this.todoArray.push({ text: this.word, done: false });
+        this.word = "";
+        this.filterArray();
+      }
+    },
+    removeItem: function(idx) {
+      let prevWordArray = [...this.todoArray];
+      prevWordArray.splice(idx, 1);
+      this.todoArray = prevWordArray;
+      this.filterArray();
+    },
+    changeStatus: function(itm) {
+      let clickedElem = this.todoArray.find(item => {
+        return item.text === itm.text;
+      });
+      clickedElem.done = clickedElem.done === false;
+      this.filterArray();
+    },
+    filterArray: function(type) {
+        if (type === "active") {
+            this.filteredArray = this.todoArray.filter(item => {
+                return item.done === false;
+            })
+        } else if (type === 'complete'){
+            this.filteredArray = this.todoArray.filter(item => {
+                return item.done === true;
+            })
+        } else {
+            this.filteredArray = this.todoArray;
+        }
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.listGroup {
+  width: 30%;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 20px;
+}
+
+span {
+  color: purple;
+  font-weight: bold;
+}
+
+input[type="text"] {
+  margin-top: 20px;
+  padding: 8px 8px 8px 14px;
+  border-radius: 5px;
+  border: 3px solid lightblue;
+}
+
+.listGroupItem {
+  margin-top: 10px;
+  text-align: left;
+}
+
+.addButton {
+  margin-left: 10px;
+  padding: 5px 10px;
+  background-color: lightgreen;
+  border: 1px solid lightgreen;
+  color: white;
+  border-radius: 3px;
+}
+
+.removeButton {
+  color: white;
+  font-weight: bold;
+  background-color: red;
+  border: 1px solid red;
+  font-size: 14px;
+  height: 30px;
+  width: 30px;
+  border-radius: 50%;
+  float: right;
+  transition: background-color 0.5s, color 0.5s;
+}
+
+.checkIcon {
+  float: right;
+  font-size: 28px;
+  margin-right: 10px;
+  color: green;
+}
+
+.checkIcon:hover {
+  cursor: pointer;
+}
+
+.removeButton:hover {
+  cursor: pointer;
+  background-color: white;
+  color: red;
+}
+
+.active {
+  text-decoration-line: line-through;
+  text-decoration-color: black;
+  background-color: gainsboro;
+  border-color: gainsboro;
+  color: gray;
+}
+
+.filter {
+  margin-top: 15px;
+}
+
+.filter li {
+  display: inline;
+  margin-right: 15px;
+}
+</style>
